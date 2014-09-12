a3789ea92cb711617e29f6ae3696c60b522b0fed
Kim Daniel Arthur <kim@kimdanielarthur.com>
2014-09-13 01:31:54 +0200

    playing with actors

--- core/src/net/peterme/agj/ArcticGameJam.java
+++ core/src/net/peterme/agj/ArcticGameJam.java
@@ -27,7 +27,7 @@ public class ArcticGameJam extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		stage.act(Gdx.graphics.getDeltaTime());
 		stage.draw();
